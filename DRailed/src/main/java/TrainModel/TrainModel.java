f46e0633c5f213440a78726345b43c15f7b6dcd3
JunHao Huang <junhaohuang@JunhaodeMacBook-Pro.local>
2017-04-05 23:31:59 -0400

    lsfaj;lsdkf

--- DRailed/src/main/java/TrainModel/TrainModel.java
+++ DRailed/src/main/java/TrainModel/TrainModel.java
@@ -266,15 +266,14 @@ public class TrainModel {
 
 //        Train train = new Train();
         int i = 10000;
-<<<<<<< HEAD
+
 
        // while(i > 0){
   //          train.calculateSpeed(1000.0);
 
-=======
+
        // while(i > 0){
   //          train.calculateSpeed(1000.0);
->>>>>>> master
         //while(i > 0){
             //train.calculateSpeed(1000.0);
             /*Double velocity = train.TimeCalc();
@@ -286,10 +285,8 @@ public class TrainModel {
             i--;
             grid.add(speedLabelText, 0, 1);
             */
-<<<<<<< HEAD
       //  }
-=======
->>>>>>> master
+
 
     }
 }
